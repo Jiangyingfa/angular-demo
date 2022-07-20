2ed24d3b05e7d0970ea71597b37895e9f46baa7b
Jane Jiang <Jane_Y_Jiang@manulife.com>
2022-07-20 15:09:22 +0800

    web worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  
+  constructor(){
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker(new URL('./app.worker', import.meta.url));
+      worker.onmessage = ({ data }) => {
+        console.log(`page got message: ${data}`);
+        localStorage.setItem("msg", data);
+      };
+      worker.postMessage('hello');
+    } else {
+      // Web workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+
+    console.log(`localStorage: `+localStorage.getItem('msg'));
+  }
 }
